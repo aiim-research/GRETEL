91b4a34d97cde26fc66a27532b94e62f0c56292b
giovanni <g.stilo@gmail.com>
2024-01-02 23:32:25 +0100

    launchers

--- launchers/glaunch.sh
+++ launchers/glaunch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -pe mpi 1
+#$ -pe mpi 16
 #$ -cwd
 #$ -o ./output/qsub/std_$JOB_ID.out
 #$ -e ./output/qsub/err_$JOB_ID.out
@@ -9,5 +9,11 @@ mkdir -p ./output/qsub
 
 export CUDA_VISIBLE_DEVICES=$1
 echo $CUDA_VISIBLE_DEVICES
+
+export OMP_NUM_THREADS=$NSLOTS
+export OPENBLAS_NUM_THREADS=$NSLOTS
+export MKL_NUM_THREADS=$NSLOTS
+export VECLIB_MAXIMUM_THREADS=$NSLOTS
+export NUMEXPR_NUM_THREADS=$NSLOTS
 
 python $2 $3 $4
--- launchers/launch.sh
+++ launchers/launch.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 #$ -S /bin/bash
-#$ -pe mpi 4
+#$ -pe mpi 16
 #$ -cwd
 #$ -o ./output/qsub/std_$JOB_ID.out
 #$ -e ./output/qsub/err_$JOB_ID.out
 #$ -q parallel.q
 mkdir -p ./output/qsub
+
+export OMP_NUM_THREADS=$NSLOTS
+export OPENBLAS_NUM_THREADS=$NSLOTS
+export MKL_NUM_THREADS=$NSLOTS
+export VECLIB_MAXIMUM_THREADS=$NSLOTS
+export NUMEXPR_NUM_THREADS=$NSLOTS
 
 python $1 $2 $3
