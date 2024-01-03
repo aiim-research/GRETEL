15778cbf426ee61fc4129ec6b789adb7ffb28d8d
giovanni-stilo <g.stilo@gmail.com>
2024-01-03 16:52:12 +0100

    launchers w scope

--- launchers/experiments.sh
+++ launchers/experiments.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-search_dir=./config/
+type='sge' # sge | local
+search_dir=./config
+trgt_scope='examples_configs'
 
-for entry in "$search_dir"/*
+for entry in "$search_dir"/*.json*
 do
-  qsub launchers/launch.sh main.py $entry
+  dcl_scope=$(python -c "import sys, json; from jsonc_parser.parser import JsoncParser; print(JsoncParser.parse_file('$entry')['experiment']['scope'])")
+
+  if [ "$trgt_scope" == "$dcl_scope" ]; then
+    echo "Founded $dcl_scope in $entry"
+    if [ "$type" == "sge" ]; then
+      echo "qsub launchers/launch.sh main.py $entry"
+    else
+      echo "python main.py $entry"
+    fi
+  fi
+  #
 done
--- launchers/local_multi.sh
+++ launchers/local_multi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Executes several (sequential) runs of the same configuration
 
-cfg=config/TCR-500-28-0.3_GCN_RSGG.jsonc
-num_runs=33
+cfg=config/TCR-500-64-0.4_GCN_RSGG.jsonc
+num_runs=1
 
 for i in {1..$num_runs}
 do
