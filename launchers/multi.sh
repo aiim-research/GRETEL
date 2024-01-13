b676b47189e27407c95353bf1b1f44a3c63b3487
giovanni <g.stilo@gmail.com>
2024-01-13 17:49:31 +0100

    multi launcher

--- launchers/multi.sh
+++ launchers/multi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Schedule multiple runs of all the configurations contained in the searchdir
 
-search_dir=./config/explainers/cf2
-num_runs=1
+search_dir=./config/_papers/ijcai24/main
+num_runs=33
 
 for i in {1..$num_runs}
 do
-    for entry in "$search_dir"/*
+    for entry in "$search_dir"/*.jsonc
     do
         qsub launchers/launch.sh main.py $entry $i
     done
