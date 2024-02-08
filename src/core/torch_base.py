677aee9e258c72744b2363c5d5de372054b4014f
czajkowskis <szymonczajkowski022@gmail.com>
2024-02-08 03:33:01 +0100

    Prototype of hyperparameter optimization

--- src/core/torch_base.py
+++ src/core/torch_base.py
@@ -6,11 +6,15 @@ from src.core.factory_base import get_instance_kvargs
 from sklearn.metrics import accuracy_score
 import torch.optim.lr_scheduler as lr_scheduler
 
+import optuna
+from src.oracle.nn.gcn import DownstreamGCN
+
 class TorchBase(Trainable):
        
     def init(self):
         self.epochs = self.local_config['parameters']['epochs']
         self.batch_size = self.local_config['parameters']['batch_size']
+        self.optimize_hyperparameters_GCN = self.local_config['parameters']['optimize_hyperparameters_GCN']
         
         self.model = get_instance_kvargs(self.local_config['parameters']['model']['class'],
                                    self.local_config['parameters']['model']['parameters'])
@@ -33,35 +37,65 @@ class TorchBase(Trainable):
         self.model.to(self.device)                            
     
     def real_fit(self):
-              
-        loader = self.dataset.get_torch_loader(fold_id=self.fold_id, batch_size=self.batch_size, usage='train')
-        
-        for epoch in range(self.epochs):
-            losses = []
-            preds = []
-            labels_list = []
-            for batch in loader:
-                batch.batch = batch.batch.to(self.device)
-                node_features = batch.x.to(self.device)
-                edge_index = batch.edge_index.to(self.device)
-                edge_weights = batch.edge_attr.to(self.device)
-                labels = batch.y.to(self.device).long()
+        if self.optimize_hyperparameters_GCN:
+            self.model = self.find_and_set_best_GCN()
+            loader = self.dataset.get_torch_loader(fold_id=self.fold_id, batch_size=self.batch_size, usage='train')
+            
+            for epoch in range(self.epochs):
+                losses = []
+                preds = []
+                labels_list = []
+                for batch in loader:
+                    batch.batch = batch.batch.to(self.device)
+                    node_features = batch.x.to(self.device)
+                    edge_index = batch.edge_index.to(self.device)
+                    edge_weights = batch.edge_attr.to(self.device)
+                    labels = batch.y.to(self.device).long()
+                    
+                    self.optimizer.zero_grad()
+                    
+                    pred = self.model(node_features, edge_index, edge_weights, batch.batch)
+                    loss = self.loss_fn(pred, labels)
+                    losses.append(loss.to('cpu').detach().numpy())
+                    loss.backward()
+                    
+                    labels_list += list(labels.squeeze().long().detach().to('cpu').numpy())
+                    preds += list(pred.squeeze().detach().to('cpu').numpy())
                 
-                self.optimizer.zero_grad()
-                
-                pred = self.model(node_features, edge_index, edge_weights, batch.batch)
-                loss = self.loss_fn(pred, labels)
-                losses.append(loss.to('cpu').detach().numpy())
-                loss.backward()
-                
-                labels_list += list(labels.squeeze().long().detach().to('cpu').numpy())
-                preds += list(pred.squeeze().detach().to('cpu').numpy())
-               
-                self.optimizer.step()
+                    self.optimizer.step()
 
-            accuracy = self.accuracy(labels_list, preds)
-            self.context.logger.info(f'epoch = {epoch} ---> loss = {np.mean(losses):.4f}\t accuracy = {accuracy:.4f}')
-            self.lr_scheduler.step()
+                accuracy = self.accuracy(labels_list, preds)
+                self.context.logger.info(f'epoch = {epoch} ---> loss = {np.mean(losses):.4f}\t accuracy = {accuracy:.4f}')
+                self.lr_scheduler.step()
+        else:
+            loader = self.dataset.get_torch_loader(fold_id=self.fold_id, batch_size=self.batch_size, usage='train')
+            
+            for epoch in range(self.epochs):
+                losses = []
+                preds = []
+                labels_list = []
+                for batch in loader:
+                    batch.batch = batch.batch.to(self.device)
+                    node_features = batch.x.to(self.device)
+                    edge_index = batch.edge_index.to(self.device)
+                    edge_weights = batch.edge_attr.to(self.device)
+                    labels = batch.y.to(self.device).long()
+                    
+                    self.optimizer.zero_grad()
+                    
+                    pred = self.model(node_features, edge_index, edge_weights, batch.batch)
+                    loss = self.loss_fn(pred, labels)
+                    losses.append(loss.to('cpu').detach().numpy())
+                    loss.backward()
+                    
+                    labels_list += list(labels.squeeze().long().detach().to('cpu').numpy())
+                    preds += list(pred.squeeze().detach().to('cpu').numpy())
+                
+                    self.optimizer.step()
+
+                accuracy = self.accuracy(labels_list, preds)
+                self.context.logger.info(f'epoch = {epoch} ---> loss = {np.mean(losses):.4f}\t accuracy = {accuracy:.4f}')
+                self.lr_scheduler.step()
             
     def check_configuration(self):
         super().check_configuration()
@@ -69,9 +103,11 @@ class TorchBase(Trainable):
         # set defaults
         local_config['parameters']['epochs'] = local_config['parameters'].get('epochs', 200)
         local_config['parameters']['batch_size'] = local_config['parameters'].get('batch_size', 4)
+        local_config['parameters']['optimize_hyperparameters_GCN'] = local_config['parameters'].get('optimize_hyperparameters_GCN', False)
         # populate the optimizer
         init_dflts_to_of(local_config, 'optimizer', 'torch.optim.Adam',lr=0.001)
         init_dflts_to_of(local_config, 'loss_fn', 'torch.nn.BCELoss')
+    
         
     def accuracy(self, testy, probs):
         acc = accuracy_score(testy, np.argmax(probs, axis=1))
@@ -92,3 +128,48 @@ class TorchBase(Trainable):
             for model in self.model:
                 if isinstance(model, torch.nn.Module):
                     model.to(self.device)
+
+    # hyperparameter search related functions
+    def optuna_objective_GCN(self, trial):
+        num_conv_layers = trial.suggest_int("num_conv_layers",2,10,log=True)
+        num_dense_layers = trial.suggest_int("num_dense_layers",2,10,log=True)
+        conv_booster = trial.suggest_int("conv_booster",1,5,log=True)
+        linear_decay = trial.suggest_int("linear_decay",1,5,log=True)
+        model_parameters = {"num_conv_layers": num_conv_layers, "num_dense_layers": num_dense_layers, "conv_booster": conv_booster, "linear_decay": linear_decay, "node_features": 7, "n_classes": 2}
+        model= get_instance_kvargs(self.local_config['parameters']['model']['class'],
+                                     model_parameters)
+
+        loader = self.dataset.get_torch_loader(fold_id=self.fold_id, batch_size=self.batch_size, usage='train')
+            
+        for epoch in range(self.epochs):
+            losses = []
+            preds = []
+            labels_list = []
+            for batch in loader:
+                batch.batch = batch.batch.to(self.device)
+                node_features = batch.x.to(self.device)
+                edge_index = batch.edge_index.to(self.device)
+                edge_weights = batch.edge_attr.to(self.device)
+                labels = batch.y.to(self.device).long()
+                
+                self.optimizer.zero_grad()
+                
+                pred = model(node_features, edge_index, edge_weights, batch.batch)
+                loss = self.loss_fn(pred, labels)
+                losses.append(loss.to('cpu').detach().numpy())
+                loss.backward()
+                
+                labels_list += list(labels.squeeze().long().detach().to('cpu').numpy())
+                preds += list(pred.squeeze().detach().to('cpu').numpy())
+            
+                self.optimizer.step()
+
+            accuracy = self.accuracy(labels_list, preds)
+            self.context.logger.info(f'epoch = {epoch} ---> loss = {np.mean(losses):.4f}\t accuracy = {accuracy:.4f}')
+            self.lr_scheduler.step()
+        return np.mean(losses)
+    
+    def find_and_set_best_GCN(self):
+        study = optuna.create_study(study_name="GCN optimization")
+        study.optimize(self.optuna_objective_GCN, n_trials=10)
+        return DownstreamGCN(num_conv_layers = study.best_params['num_conv_layers'], num_dense_layers = study.best_params['num_dense_layers'], conv_booster=study.best_params['conv_booster'], linear_decay=study.best_params['linear_decay'], node_features=7, n_classes=2)
