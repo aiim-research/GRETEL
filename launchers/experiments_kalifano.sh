8622a756d65741883d59ea42dc7e286767a48a00
dangeloandrea14 <dangeloandrea14@gmail.com>
2024-05-03 14:10:17 +0200

    generate_results script

--- launchers/experiments_kalifano.sh
+++ launchers/experiments_kalifano.sh
@@ -3,16 +3,19 @@ type='sge' # sge | local
 search_dir=$1 # the dir to search
 trgt_scope='methods_comparison' # scope to be considered
 
-for entry in "$search_dir"/*/*.json*
+for i in {1..30}
 do
-  dcl_scope=$(python -c "import sys, json; from jsonc_parser.parser import JsoncParser; print(JsoncParser.parse_file('$entry')['experiment']['scope'])")
+  for entry in "$search_dir"/*/*.json*
+  do
+    dcl_scope=$(python -c "import sys, json; from jsonc_parser.parser import JsoncParser; print(JsoncParser.parse_file('$entry')['experiment']['scope'])")
 
-  if [ "$trgt_scope" == "$dcl_scope" ]; then
-    echo "Founded $dcl_scope in $entry"
-    if [ "$type" == "sge" ]; then
-      sbatch launchers/launch.sh main.py $entry
-    else
-      python main.py $entry
+    if [ "$trgt_scope" == "$dcl_scope" ]; then
+      echo "Founded $dcl_scope in $entry"
+      if [ "$type" == "sge" ]; then
+        sbatch launchers/launch.sh main.py $entry $i
+      else
+        python main.py $entry $i
+      fi
     fi
-  fi
+  done
 done
