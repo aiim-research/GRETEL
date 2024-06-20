2755da29301fd42966cd686da78f49ff85d77bec
Leynier Gutiérrez González <leynier41@gmail.com>
2024-06-20 16:19:11 +0200

    fix: move functions about multi criteria outside of class

--- src/explainer/ensemble/aggregators/multi_criteria.py
+++ src/explainer/ensemble/aggregators/multi_criteria.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Callable, List
 
 import numpy as np
 
@@ -48,13 +48,13 @@ class ExplanationMultiCriteriaAggregator(ExplanationAggregator):
             ),
             np.array([criteria.gain_direction().value for criteria in self.criterias]),
         )
-        criteria_matrix_normalized = self.__min_max_normalize(criteria_matrix)
         gain_directions = np.array(
             [criteria.gain_direction().value for criteria in self.criterias]
         )
-        best_index = self.__find_best(
-            criteria_matrix_normalized,
+        best_index = __find_best(
+            criteria_matrix,
             gain_directions,
+            self.distance.calculate,
         )
         best_cf = cf_instances[best_index]
         return LocalGraphCounterfactualExplanation(
@@ -66,61 +66,66 @@ class ExplanationMultiCriteriaAggregator(ExplanationAggregator):
             counterfactual_instances=[best_cf],
         )
 
-    def __min_max_normalize(self, criteria_matrix: np.ndarray) -> np.ndarray:
-        min_vals = np.min(criteria_matrix, axis=0)
-        max_vals = np.max(criteria_matrix, axis=0)
-        normalized_matrix = (criteria_matrix - min_vals) / (max_vals - min_vals)
-        return normalized_matrix
 
-    def __find_non_dominated_rows(
-        self,
-        criteria_matrix: np.ndarray,
-        gain_directions: np.ndarray,
-    ) -> np.ndarray:
-        criteria_matrix_normalized = criteria_matrix * gain_directions
-        num_rows = criteria_matrix.shape[0]
-        non_dominated_indices = []
-        for i in range(num_rows):
-            dominated = False
-            for j in range(num_rows):
-                row1 = criteria_matrix_normalized[i]
-                row2 = criteria_matrix_normalized[j]
-                if i != j and np.all(row2 >= row1) and np.any(row2 > row1):
-                    dominated = True
-                    break
-            if not dominated:
-                non_dominated_indices.append(i)
-        return np.array(non_dominated_indices)
+def __min_max_normalize(
+    criteria_matrix: np.ndarray,
+) -> np.ndarray:
+    min_vals = np.min(criteria_matrix, axis=0)
+    max_vals = np.max(criteria_matrix, axis=0)
+    normalized_matrix = (criteria_matrix - min_vals) / (max_vals - min_vals)
+    return normalized_matrix
 
-    def __compute_ideal_point(
-        self,
-        criteria_matrix: np.ndarray,
-        non_dominated_indices: np.ndarray,
-        gain_directions: np.ndarray,
-    ) -> np.ndarray:
-        non_dominated_matrix = criteria_matrix[non_dominated_indices]
-        ideal_point = (
-            np.max(non_dominated_matrix * gain_directions, axis=0) * gain_directions
-        )
-        return ideal_point
 
-    def __find_best(
-        self,
-        criteria_matrix: np.ndarray,
-        gain_directions: np.ndarray,
-    ) -> int:
-        non_dominated_indices = self.__find_non_dominated_rows(
-            criteria_matrix,
-            gain_directions,
-        )
-        ideal_point = self.__compute_ideal_point(
-            criteria_matrix,
-            non_dominated_indices,
-            gain_directions,
-        )
-        distances = self.distance.calculate(
-            criteria_matrix[non_dominated_indices],
-            ideal_point,
-        )
-        best_index = non_dominated_indices[np.argmin(distances)]
-        return best_index
+def __find_non_dominated_rows(
+    criteria_matrix: np.ndarray,
+    gain_directions: np.ndarray,
+) -> np.ndarray:
+    criteria_matrix_normalized = criteria_matrix * gain_directions
+    num_rows = criteria_matrix.shape[0]
+    non_dominated_indices = []
+    for i in range(num_rows):
+        dominated = False
+        for j in range(num_rows):
+            row1 = criteria_matrix_normalized[i]
+            row2 = criteria_matrix_normalized[j]
+            if i != j and np.all(row2 >= row1) and np.any(row2 > row1):
+                dominated = True
+                break
+        if not dominated:
+            non_dominated_indices.append(i)
+    return np.array(non_dominated_indices)
+
+
+def __compute_ideal_point(
+    criteria_matrix: np.ndarray,
+    non_dominated_indices: np.ndarray,
+    gain_directions: np.ndarray,
+) -> np.ndarray:
+    non_dominated_matrix = criteria_matrix[non_dominated_indices]
+    ideal_point = (
+        np.max(non_dominated_matrix * gain_directions, axis=0) * gain_directions
+    )
+    return ideal_point
+
+
+def __find_best(
+    criteria_matrix: np.ndarray,
+    gain_directions: np.ndarray,
+    distance_func: Callable[[np.ndarray, np.ndarray], np.ndarray],
+) -> int:
+    criteria_matrix_normalized = __min_max_normalize(criteria_matrix)
+    non_dominated_indices = __find_non_dominated_rows(
+        criteria_matrix_normalized,
+        gain_directions,
+    )
+    ideal_point = __compute_ideal_point(
+        criteria_matrix_normalized,
+        non_dominated_indices,
+        gain_directions,
+    )
+    distances = distance_func(
+        criteria_matrix_normalized[non_dominated_indices],
+        ideal_point,
+    )
+    best_index = non_dominated_indices[np.argmin(distances)]
+    return best_index
