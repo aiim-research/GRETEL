2411ec7078373a75799ec992e38e2d7ac7675fbf
Leynier Gutiérrez González <leynier41@gmail.com>
2024-06-28 16:03:22 +0200

    feat: add fidelity criteria and refactoring fidelity metrics implementations

--- src/evaluation/metrics/fidelity.py
+++ src/evaluation/metrics/fidelity.py
@@ -1,5 +1,6 @@
 from src.evaluation.metrics.base import EvaluationMetric
 from src.explanation.local.graph_counterfactual import LocalGraphCounterfactualExplanation
+from src.utils.metrics.fidelity import fidelity_metric_with_predictions
 
 
 class FidelityMetric(EvaluationMetric):
@@ -11,31 +12,20 @@ class FidelityMetric(EvaluationMetric):
         super().check_configuration()
         self.logger= self.context.logger
 
-
     def init(self):
         super().init()
         self.name = 'Fidelity'
-
 
     def evaluate(self, explanation: LocalGraphCounterfactualExplanation):
         oracle = explanation.oracle
         input_instance = explanation.input_instance
         lbl_input_instance = oracle.predict(input_instance)
         oracle._call_counter -= 1
-
-        # This term indicates how reliable is the oracle prediction
-        prediction_fidelity = 1 if (lbl_input_instance == input_instance.label) else 0
-
         aggregated_fidelity = 0
         num_instances = len(explanation.counterfactual_instances)
-
         for cf in explanation.counterfactual_instances:
             label_cf = oracle.predict(cf)
             oracle._call_counter -= 1
-
-            counterfactual_fidelity = 1 if (label_cf == input_instance.label) else 0
-            cf_fidelity = prediction_fidelity - counterfactual_fidelity
-
+            cf_fidelity = fidelity_metric_with_predictions(input_instance, lbl_input_instance, label_cf)
             aggregated_fidelity += cf_fidelity
-        
         return aggregated_fidelity / num_instances
