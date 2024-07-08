ce323e40b7865a693d4bb22b2ed447f216cddeef
Leynier Gutiérrez González <leynier41@gmail.com>
2024-07-08 14:03:45 +0200

    chore: revert changes

--- launchers/launch.sh
+++ launchers/launch.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-#SBATCH -o /NFSHOME/lgutierrez/projects/GRETEL/output/multi-criteria-logs.out
+#!/bin/bash -l
+#SBATCH -o output/logs/%j.out
+#SBATCH -s
 #SBATCH -N 1
 #SBATCH -n 1
+#SBATCH -J RSGG-MAIN
 #SBATCH -c 8
-#SBATCH -J multi-criteria-test
 #SBATCH -p normal
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
-
-srun python "$@"
